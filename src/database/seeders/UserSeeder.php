db66af171826f13c89105b37126e4e813c2dd9f3
root <root@LAPTOP-TMQ82LHU.localdomain>
2026-07-08 00:16:44 +0700

    update role

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     {
         $user = User::firstOrCreate(
             ['email' => 'admin@admin.com'],
-            ['name' => 'Super Admin', 'password' => Hash::make('password')]
+            ['name' => 'Super Admin', '12345' => Hash::make('password')]
         );
         $user->assignRole('super_admin');
 
