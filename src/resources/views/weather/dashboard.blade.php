a0d2a68cca38af768992060bbf41384b623717dd
root <root@LAPTOP-TMQ82LHU.localdomain>
2026-07-11 12:32:01 +0700

    weather card update

--- src/resources/views/weather/dashboard.blade.php
+++ src/resources/views/weather/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#071426">
-    <title>Weather Intelligence Dashboard</title>
+    <title>Weather Insight</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
